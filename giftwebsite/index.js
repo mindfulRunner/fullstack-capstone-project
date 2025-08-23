94599ffb03d40a0fc02a8e4ac6b39296e7f96cd9
Ping <pingpinganan@gmail.com>
2025-08-22 18:42:44 -0700

    workaround on invalid token error for app.get('/*', ...)

--- giftwebsite/index.js
+++ giftwebsite/index.js
@@ -4,7 +4,16 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/*', function (req, res) {
+// app.get('/*', function (req, res) {
+//
+//    need to change the above '/*' to the below '/*\w'
+//    to get around the error:
+//
+//      TypeError: Invalid token at 1: https://git.new/pathToRegexpError
+//
+//    refer to: https://stackoverflow.com/questions/78973586/typeerror-invalid-token-at-1-https-git-new-pathtoregexperror
+//
+app.get('/*\w', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
